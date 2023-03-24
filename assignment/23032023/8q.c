0bd7872c3e15178195b79004ac747356a46346f4
Niranjan Rajkumar <niranjanr0682.sse@saveetha.com>
2023-03-24 13:41:31 +0530

    updated

--- assignment/23032023/8q.c
+++ assignment/23032023/8q.c
@@ -12,12 +12,10 @@ int main() {
         scanf("%d", &arr[i]);
     printf("Enter the element to search: ");
     scanf("%d", &x);
-    for(i=0; i<n; i++) {
-        if(arr[i] == x) {
-            printf("%d is found at index %d.\n", x, i);
-            return 0;
-        }
-    }
+    for(i=0; i<n; i++) 
+        {if(arr[i] == x)
+            {printf("%d is found at index %d.\n", x, i);
+            return 0;}}
     printf("-1 the %d is not present in the array.\n", x);
     return 0;
 }
